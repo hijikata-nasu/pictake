8e292302d95e4ea3586a915edc0b23e71ed7322d
cmyUE <kaitomikkumiku+github@gmail.com>
2019-10-15 01:26:58 +0900

    Initial commit BottomNavigationBerとFaBを実装した。 レイアウトだけだけど。

---
+++ app/src/main/java/com/onct_ict/azukimattya/pictake/MapFragment.kt
@@ -0,0 +1,16 @@
+package com.onct_ict.azukimattya.pictake
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MapFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return  inflater.inflate(R.layout.fragment_map, container, false)
+    }
+}
